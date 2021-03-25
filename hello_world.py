3d4227a2c8e2c8de682a81a74da1ac898473aaaf
Sudhir Singh <sudhir.13singh@gmail.com>
2021-03-24 23:11:51 -0400

    travis ci

--- hello_world.py
+++ hello_world.py
@@ -6,68 +6,70 @@
 import requests, json
 import os
 
-# Enter your API key here
-api_key = os.environ.get('SECRET_KEY')
 
-# base_url variable to store url
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
+def hello_world(city_name):
+    # Enter your API key here
+    api_key = os.environ.get('SECRET_KEY')
 
-# Give city name
-city_name = input("Enter city name : ")
+    # base_url variable to store url
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-# complete_url variable to store
-# complete url address
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    # complete_url variable to store
+    # complete url address
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
 
-# get method of requests module
-# return response object
-response = requests.get(complete_url)
+    # get method of requests module
+    # return response object
+    response = requests.get(complete_url)
 
-# json method of response object
-# convert json format data into
-# python format data
-x = response.json()
+    # json method of response object
+    # convert json format data into
+    # python format data
+    x = response.json()
 
-# Now x contains list of nested dictionaries
-# Check the value of "cod" key is equal to
-# "404", means city is found otherwise,
-# city is not found
-if x["cod"] != "404":
+    # Now x contains list of nested dictionaries
+    # Check the value of "cod" key is equal to
+    # "404", means city is found otherwise,
+    # city is not found
+    msg = "City Not Found"
 
-	# store the value of "main"
-	# key in variable y
-	y = x["main"]
+    if x["cod"] != "404":
 
-	# store the value corresponding
-	# to the "temp" key of y
-	current_temperature = y["temp"]
+        # store the value of "main"
+        # key in variable y
+        y = x["main"]
 
-	# store the value corresponding
-	# to the "pressure" key of y
-	current_pressure = y["pressure"]
+        # store the value corresponding
+        # to the "temp" key of y
+        current_temperature = y["temp"]
 
-	# store the value corresponding
-	# to the "humidity" key of y
-	current_humidiy = y["humidity"]
+        # store the value corresponding
+        # to the "pressure" key of y
+        current_pressure = y["pressure"]
 
-	# store the value of "weather"
-	# key in variable z
-	z = x["weather"]
+        # store the value corresponding
+        # to the "humidity" key of y
+        current_humidiy = y["humidity"]
 
-	# store the value corresponding
-	# to the "description" key at
-	# the 0th index of z
-	weather_description = z[0]["description"]
+        # store the value of "weather"
+        # key in variable z
+        z = x["weather"]
 
-	# print following values
-	print(" Temperature (in kelvin unit) = " +
-					str(current_temperature) +
-		"\n atmospheric pressure (in hPa unit) = " +
-					str(current_pressure) +
-		"\n humidity (in percentage) = " +
-					str(current_humidiy) +
-		"\n description = " +
-					str(weather_description))
+        # store the value corresponding
+        # to the "description" key at
+        # the 0th index of z
+        weather_description = z[0]["description"]
 
-else:
-	print(" City Not Found ")
+        # print following values
+        msg = "Temperature = " + str(current_temperature)
+        print(msg)
+        return msg
+
+    else:
+        print("City Not Found")
+        return msg
+
+
+if __name__ == '__main__':
+    city_name = input("Enter city name : ")
+    hello_world(city_name)
---
+++ test_hello_world.py
@@ -0,0 +1,8 @@
+import hello_world
+import pytest
+
+
+def test_hello_world():
+    city = "Chicago"
+    assert "Temperature" in hello_world.hello_world(city) == True
+
