d942f2e3171fc33aa5cd23b797f3d288957d2665
Sudhir Singh <sudhir.13singh@gmail.com>
2021-03-24 22:06:27 -0400

    Update hw.py

--- hw.py
+++ hw.py
@@ -1,7 +1,8 @@
 
 def hello_world():
-  print("hello world!")
+    print("hello world!")
+    return "hello world!"
 
 
-if __name__ = "__main__"
-  print(hello_world)
+if __name__ == "__main__":
+    print(hello_world)
